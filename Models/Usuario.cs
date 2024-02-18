6bb8feb589296db500169396c996510d6c916201
samuelerm08 <samuelerm08@gmail.com>
2024-02-18 02:48:26 -0300

    Remove unused regexs and required field validations

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -11,20 +11,14 @@ namespace SistemaFinanciero.WebApi.Models
         public int Id { get; set; }
         [Column(TypeName = "varchar(30)")]
         [Required(ErrorMessage = "Campo Obligatorio...")]
-        //Se permiten dos nombres. El segundo es opcional.
-        [RegularExpression(@"^[A-Za-z]{1,15}( [A-Za-z]{1,15})?$", ErrorMessage = "El máximo permitido por nombre es de 15 caracteres.")]
         public string Nombre { get; set; }
         [Column(TypeName = "varchar(30)")]
         [Required(ErrorMessage = "Campo Obligatorio...")]
-        //Se permiten dos apellidos. El segundo es opcional.
-        [RegularExpression(@"^[A-Za-z]{1,15}( [A-Za-z]{1,15})?$", ErrorMessage = "El máximo permitido por apellido es de 15 caracteres.")]
         public string Apellido { get; set; }
         [Column(TypeName = "varchar(30)")]
-        [Required(ErrorMessage = "Campo Obligatorio...")]
         [RegularExpression(@"^[A-Za-z0-9._-]{1,15}@[A-Za-z0-9.-]{1,15}\.[A-Za-z]{2,5}(\.[A-Za-z]{2,5})?$", ErrorMessage = "La dirección de correo electrónico no es válida.")]
         public string Email { get; set; }
         [Column(TypeName = "varchar(80)")]
-        [Required(ErrorMessage = "Campo Obligatorio...")]
         public string Contraseña { get; set; }
         [Required(ErrorMessage = "Campo Obligatorio...")]
         public bool EsAdmin { get; set; }
